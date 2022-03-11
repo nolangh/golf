709e4f57f6d105e8843f1378eda57d5450d241ce
nolangh <nolanghiggins@gmail.com>
2022-03-10 17:30:59 -0700

    -a

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
-const myCourses = [];
+let myCourses = "";
 
 function getAvailableCourses() {
-	fetch("https://golf-courses-api.herokuapp.com/courses", {
-		method: "get",
-	})
+	fetch("https://golf-courses-api.herokuapp.com/courses")
 		.then((response) => {
 			return response.json();
 		})
 		.then((info) => {
-			console.log(info);
 			myCourses = info; //try seeing if I can declare the variable in this statment
+			console.log(info);
 		})
 		.then(() => {
-			showCourse();
+			selectCourse();
 		});
 }
 
-function showCourse() {
-	let courseOptionsHtml;
-	courseOptionsHtml.forEach((course) => {
+function selectCourse() {
+	let courseOptionsHtml = "";
+	let courseImages = "";
+	myCourses.courses.forEach((course) => {
 		courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+		courseImages += `<h4> ${course.name}</h4><img id= 'course-image' src= '${course.image}'>`; //this will go into the show course function
 	});
+
 	document.getElementById("course-select").innerHTML = courseOptionsHtml;
+	document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
 }
+
+getAvailableCourses();
