28dcaa1f212644868e0205c3e4648c9973093295
nolangh <nolanghiggins@gmail.com>
2022-03-11 11:14:16 -0700

    -a

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 export { getAvailableCourses, getCourseImage };
 
 let myCourses = "";
+let courseImages = "";
 /* --------------------- ANCHOR request api information --------------------- */
 function getAvailableCourses() {
 	fetch("https://golf-courses-api.herokuapp.com/courses")
@@ -21,20 +22,13 @@ function getAvailableCourses() {
 /* ----------- ANCHOR assigns api information to html information ----------- */
 function selectCourse() {
 	let courseOptionsHtml = "";
-
 	myCourses.courses.forEach((course) => {
 		courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+		courseImages += `<h4>${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
 	});
-	document.addEventListener("click", getCourseImage);
 	document.getElementById("course-select").innerHTML = courseOptionsHtml;
+	document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
 }
 
 /* -------------------- ANCHOR get course image on click -------------------- */
-function getCourseImage() {
-	let courseImages = "";
-	myCourses.courses((course) => {
-		//add new variable that is equal to the object ID
-		courseImages += `<h4> ${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
-	});
-	document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
-}
+function getCourseImage() {}
