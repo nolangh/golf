d9aadf181e2016277c7990f9c107c00e420a90d9
nolangh <nolanghiggins@gmail.com>
2022-04-22 18:01:12 -0600

    -a

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,10 @@
 // export { getAvailableCourses, getCourseImage };
+const select = document.getElementById("course-select");
+const images = document.getElementById("courseImage");
 
-// let myCourses = "";
+let myCourses = "";
 
-/* function getAvailableCourses() {
+function getAvailableCourses() {
 	fetch("https://golf-courses-api.herokuapp.com/courses")
 		.then((response) => {
 			//passes api into this function
@@ -11,30 +13,47 @@
 		.then((info) => {
 			//takes the .JSON and grabs the data from the .JSON
 			myCourses = info; // Assigns the .JSON data to variable myCourses
-			console.log(info);
+			console.log(myCourses);
 		})
 		.then(() => {
 			selectCourse(); // calls the select course function
 		});
-} 
+}
+
+/* os.addEventListener("change", () => {
+	courseImage = pos.options[pos.selectedIndex].value;
+	return fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${setName}`, {
+		method: "GET",
+		headers: {
+			"X-api-key": apiKey,
+		},
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((filteredSet) => {
+			allPokemon = filteredSet;
+			console.log(allPokemon);
+		})
+		.then(() => {
+			allPokemon.data.forEach((data) => {
+				createCard(data);
+			});
+		});
+}); */
 
 function selectCourse() {
 	let courseOptionsHtml = "";
 	myCourses.courses.forEach((course) => {
 		courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
-		courseImages += `<h4>${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
+		images.src = `${course.image}`;
 	});
 	// document.addEventListener("click", getCourseImage);
+	//courseImages += `<h4>${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
 	document.getElementById("course-select").innerHTML = courseOptionsHtml;
-	document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
-} */
+	courseImage = pos.options[pos.selectedIndex].value;
 
-async function getAvailableCourses() {
-	const apiData = fetch("https://golf-courses-api.herokuapp.com/courses");
-	console.log(apiData);
-	const response = await apiData.json();
-	const myCourses = await response();
-	console.log(myCourses);
+	//document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
 }
 
 getAvailableCourses();
