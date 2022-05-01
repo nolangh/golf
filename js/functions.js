ff98ae35875d897898de7e29db51f17df539ec3c
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-04-30 21:32:22 -0600

    -a

--- js/functions.js
+++ js/functions.js
@@ -20,43 +20,17 @@ function getAvailableCourses() {
 		});
 }
 
-/* os.addEventListener("change", () => {
-	courseImage = pos.options[pos.selectedIndex].value;
-	return fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${setName}`, {
-		method: "GET",
-		headers: {
-			"X-api-key": apiKey,
-		},
-	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((filteredSet) => {
-			allPokemon = filteredSet;
-			console.log(allPokemon);
-		})
-		.then(() => {
-			allPokemon.data.forEach((data) => {
-				createCard(data);
-			});
-		});
-}); */
-
 function selectCourse() {
 	let courseOptionsHtml = "";
 
 	myCourses.courses.forEach((course) => {
 		courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
 
-		console.log(courseOptionsHtml.option);
 		images.src = `${course.image}`;
 	});
 	// document.addEventListener("click", getCourseImage);
-	//courseImages += `<h4>${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
+	courseImages += `<h4>${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
 	document.getElementById("course-select").innerHTML = courseOptionsHtml;
-	courseImage = pos.options[pos.selectedIndex].value;
-
-	//document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
 }
 
 getAvailableCourses();
